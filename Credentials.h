1df02fec9b6379ff18077d99da4e25fd08d3246d
FaultyTaco <faultytaco@icloud.com>
2016-11-24 18:33:15 +1100

    Add files via upload

---
+++ Credentials.h
@@ -0,0 +1,8 @@
+// Constants.h
+#if !defined(MYLIB_CONSTANTS_H)
+#define MYLIB_CONSTANTS_H 1
+
+const char ssid[] = "xxxxxxxx";  //  your network SSID (name)
+const char pass[] = "xxxxxxxx";       // your network password
+
+#endif
